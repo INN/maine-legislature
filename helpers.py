a257868e0c132384eb059d496bc0b559ffd218fd
Ryan Nagle <rmnagle@gmail.com>
2015-10-16 14:40:00 -0500

    use a simple caching method to keep from hitting the disk each time we need the dataset

--- helpers.py
+++ helpers.py
@@ -3,15 +3,16 @@ import app_config
 import copytext
 import re
 
-from render_utils import make_context
 from unicodedata import normalize
+
+CACHE = {}
 
 _punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
 
 def get_legislators():
-    context = make_context()
-    COPY = context['COPY']
-    return COPY['senators']._sheet + COPY['house_reps']._sheet
+    copy = get_copy()
+    return copy['senators']._sheet + copy['house_reps']._sheet
+
 
 def get_legislator_slugs():
     legislators = get_legislators()
@@ -19,6 +20,7 @@ def get_legislator_slugs():
     for legislator in legislators:
         slugs.append(slugify(legislator['name']))
     return slugs
+
 
 def get_legislator_by_slug(slug):
     legislators = get_legislators()
@@ -30,18 +32,19 @@ def get_legislator_by_slug(slug):
 
     return leg
 
+
 def get_legislator_id_by_slug(slug):
     leg = get_legislator_by_slug(slug)
     return leg['id']
 
+
 # I apologize for the length of this function.
 def get_legislator_income_by_slug(slug):
-    context = make_context()
-    COPY = context['COPY']
+    copy = get_copy()
     income = {}
     leg_id = get_legislator_id_by_slug(slug)
 
-    for row in COPY['gifts']:
+    for row in copy['gifts']:
         if row['sh_number'] == leg_id:
             try:
                 income['gifts']
@@ -51,7 +54,7 @@ def get_legislator_income_by_slug(slug):
             if row['Source_of_Gift'] != '':
                 income['gifts'].append(row['Source_of_Gift'] + ' (Gifts)')
 
-    for row in COPY['honoraria']:
+    for row in copy['honoraria']:
         if row['sh_number'] == leg_id:
             try:
                 income['honoraria']
@@ -61,7 +64,7 @@ def get_legislator_income_by_slug(slug):
             if row['Source_of_Honoraria'] != '':
                 income['honoraria'].append(row['Source_of_Honoraria'] + ' (honorarium)')
 
-    for row in COPY['loans']:
+    for row in copy['loans']:
         if row['sh_number'] == leg_id:
             try:
                 income['loans']
@@ -69,13 +72,13 @@ def get_legislator_income_by_slug(slug):
                 income['loans'] = []
 
             if row['Name_of_Lender'] != '' and row['City_of_Lender'] != '' and row['Zip_of_Lender'] != '':
-                income['loans'].append( \
-                    row['Name_of_Lender'] + ', ' \
-                    + row['City_of_Lender'] + ', ' \
-                    #+ format_zip(row['Zip_of_Lender']) + ' (Loan)' \
+                income['loans'].append(
+                    row['Name_of_Lender'] + ', '
+                    + row['City_of_Lender'] + ', '
+                    # + format_zip(row['Zip_of_Lender']) + ' (Loan)'
                 )
 
-    for row in COPY['income_employmnet']:
+    for row in copy['income_employmnet']:
         if row['sh_number'] == leg_id:
             try:
                 income['income_employment']
@@ -83,14 +86,14 @@ def get_legislator_income_by_slug(slug):
                 income['income_employment'] = []
 
             if row['Name_Employer'] != '':
-                income['income_employment'].append( \
-                    row['Position'] + ', ' \
-                    + row['Name_Employer'] + ', ' \
-                    + row['Employer_City'] + ', ' \
-                    #+ format_zip(row['Employer_Zip']) \
+                income['income_employment'].append(
+                    row['Position'] + ', '
+                    + row['Name_Employer'] + ', '
+                    + row['Employer_City'] + ', '
+                    # + format_zip(row['Employer_Zip'])
                 )
 
-    for row in COPY['income_self']:
+    for row in copy['income_self']:
         if row['sh_number'] == leg_id:
             try:
                 income['income_self']
@@ -98,13 +101,13 @@ def get_legislator_income_by_slug(slug):
                 income['income_self'] = []
 
             if row['Name_of_Self_Employment_Business'] != '':
-                income['income_self'].append( \
-                    row['Name_of_Self_Employment_Business'] + ', ' \
-                    + row['City_of_Self_Employment_Business'] + ', ' \
-                    #+ format_zip(row['Zip_of_Self_Employment']) \
+                income['income_self'].append(
+                    row['Name_of_Self_Employment_Business'] + ', '
+                    + row['City_of_Self_Employment_Business'] + ', '
+                    # + format_zip(row['Zip_of_Self_Employment'])
                 )
 
-    for row in COPY['income_business']:
+    for row in copy['income_business']:
         if row['sh_number'] == leg_id:
             try:
                 income['income_business']
@@ -112,13 +115,13 @@ def get_legislator_income_by_slug(slug):
                 income['income_business'] = []
 
             if row['Name_of_Business'] != '':
-                income['income_business'].append( \
-                    row['Name_of_Business'] + ', ' \
-                    + row['City_of_Business'] + ', ' \
-                    #+ format_zip(row['Zip_of_Business']) \
+                income['income_business'].append(
+                    row['Name_of_Business'] + ', '
+                    + row['City_of_Business'] + ', '
+                    # + format_zip(row['Zip_of_Business'])
                 )
 
-    for row in COPY['income_law']:
+    for row in copy['income_law']:
         if row['sh_number'] == leg_id:
             try:
                 income['income_law']
@@ -126,14 +129,14 @@ def get_legislator_income_by_slug(slug):
                 income['income_law'] = []
 
             if row['Name_of_Practice'] != '':
-                income['income_law'].append( \
-                    row['Position_in_Practice'] + ', ' \
-                    + row['Name_of_Practice'] + ', ' \
-                    + row['City_of_Practice'] + ', ' \
-                    #+ format_zip(row['Zip_of_Practice']) \
+                income['income_law'].append(
+                    row['Position_in_Practice'] + ', '
+                    + row['Name_of_Practice'] + ', '
+                    + row['City_of_Practice'] + ', '
+                    # + format_zip(row['Zip_of_Practice'])
                 )
 
-    for row in COPY['income_other']:
+    for row in copy['income_other']:
         if row['sh_number'] == leg_id:
             try:
                 income['income_other']
@@ -143,7 +146,7 @@ def get_legislator_income_by_slug(slug):
             if row['Name_of_Source'] != '':
                 line = row['Name_of_Source']
                 line += ', ' + row['City_of_Source']
-                #line += ', ' + format_zip(row['Zip_of_Source'])
+                # line += ', ' + format_zip(row['Zip_of_Source'])
                 if row['Description_of_income_type'] != '':
                     line += " (%s)" % row['Description_of_income_type']
 
@@ -151,14 +154,13 @@ def get_legislator_income_by_slug(slug):
 
     return income
 
+
 def get_legislator_positions_by_slug(slug):
-    context = make_context()
-    COPY = context['COPY']
+    copy = get_copy()
     positions = {}
     leg_id = get_legislator_id_by_slug(slug)
 
-
-    for row in COPY['position_political']:
+    for row in copy['position_political']:
         if row['sh_number'] == leg_id:
             try:
                 positions['position_political']
@@ -167,13 +169,14 @@ def get_legislator_positions_by_slug(slug):
 
             if row['Name_of_Committee'] != '':
                 if row['Name_of_Official'] == '':
-                    # the official is the legislator, per https://github.com/INN/maine-legislature/issues/68
+                    # the official is the legislator,
+                    # per https://github.com/INN/maine-legislature/issues/68
                     positions['position_political'].append(
                         row['Title_in_Committee'] + ', ' +
                         row['Name_of_Committee']
                     )
 
-    for row in COPY['position_org']:
+    for row in copy['position_org']:
         if row['sh_number'] == leg_id:
             try:
                 positions['position_org']
@@ -186,20 +189,20 @@ def get_legislator_positions_by_slug(slug):
                 line = row['Title_in_Organization'] + ', '
                 line += row['Organization'] + ', '
                 line += row['City_of_Organization'] + ', '
-                #line += format_zip(row['Zip_of_Organization'])
+                # line += format_zip(row['Zip_of_Organization'])
                 if str(row['Compensated']).lower() == 'yes':
                     line += ' (paid position)'
                 positions['position_org'].append(line)
 
     return positions
 
+
 def get_legislator_family_by_slug(slug):
-    context = make_context()
-    COPY = context['COPY']
+    copy = get_copy()
     family = {}
     leg_id = get_legislator_id_by_slug(slug)
 
-    for row in COPY['position_org']:
+    for row in copy['position_org']:
         if row['sh_number'] == leg_id:
             try:
                 family['position_org']
@@ -208,7 +211,8 @@ def get_legislator_family_by_slug(slug):
 
             # this checks row['Relationship_to_Legislator'] to make sure it's a family
             # otherwise, this goes in family member positions
-            if str(row['Relationship_to_Legislator']).lower() == 'spouse': # The values used here are spouse and self, and '' for self.
+            # The values used here are spouse and self, and '' for self.
+            if str(row['Relationship_to_Legislator']).lower() == 'spouse':
                 line = row['Name_of_Position_Holder']
                 line += " (%s)" % str(row['Relationship_to_Legislator']).lower()
                 if row['Title_in_Organization']:
@@ -218,12 +222,12 @@ def get_legislator_family_by_slug(slug):
                 if row['City_of_Organization']:
                     line += ', ' + row['City_of_Organization']
                 # Not doing zips this app
-                #line += ', ' + format_zip(row['Zip_of_Organization'])
+                # line += ', ' + format_zip(row['Zip_of_Organization'])
                 if str(row['Compensated']).lower() == 'yes':
                     line += ' (paid position)'
                 family['position_org'].append(line)
 
-    for row in COPY['family_income_compensation']:
+    for row in copy['family_income_compensation']:
         if row['sh_number'] == leg_id:
             try:
                 family['family_income_compensation']
@@ -238,30 +242,31 @@ def get_legislator_family_by_slug(slug):
                     line += ', ' + row['Family_Member_Employers_Name']
                 if str(row['Employers_City']) != '':
                     line += ', ' + row['Employers_City']
-                #if str(row['Employers_Zip']) != '':
+                # if str(row['Employers_Zip']) != '':
                 #    line += ', ' + format_zip(row['Employers_Zip'])
                 family['family_income_compensation'].append(line)
 
-    for row in COPY['family_other_income']:
+    for row in copy['family_other_income']:
         if row['sh_number'] == leg_id:
             try:
                 family['family_other_income']
             except KeyError:
                 family['family_other_income'] = []
 
-            if str(row['Name_of_spouse']) != '': # This column is also used for other family members
+            # This column is also used for other family members
+            if str(row['Name_of_spouse']) != '':
                 line = row['Name_of_spouse']
                 if str(row['Source_of_family_member_income']) != '':
-                    line += ', ' +  row['Source_of_family_member_income']
+                    line += ', ' + row['Source_of_family_member_income']
                 if str(row['City_of_other_source']) != '':
-                    line += ', ' +  row['City_of_other_source']
-                #if str(row['Zip_of_other_source']) != '':
+                    line += ', ' + row['City_of_other_source']
+                # if str(row['Zip_of_other_source']) != '':
                 #    line += ', ' +  format_zip(row['Zip_of_other_source'])
                 if str(row['Type_of_Income']) != '':
                     line += ' (%s)' % row['Type_of_Income']
                 family['family_other_income'].append(line)
 
-    for row in COPY['position_political']:
+    for row in copy['position_political']:
         if row['sh_number'] == leg_id:
             try:
                 family['position_political']
@@ -270,7 +275,8 @@ def get_legislator_family_by_slug(slug):
 
             if row['Name_of_Committee'] != '':
                 if row['Name_of_Official'] != '':
-                    # the official is the legislator, per https://github.com/INN/maine-legislature/issues/68
+                    # the official is the legislator,
+                    # per https://github.com/INN/maine-legislature/issues/68
                     family['position_political'].append(
                         row['Name_of_Official'] + ', ' +
                         row['Title_in_Committee'] + ', ' +
@@ -279,13 +285,15 @@ def get_legislator_family_by_slug(slug):
 
     return family
 
+
 def rep_sen(id):
-    if id.startswith( 's' ):
+    if id.startswith('s'):
         return u"Sen."
-    elif id.startswith( 'h' ):
+    elif id.startswith('h'):
         return u"Rep."
     else:
         return ''
+
 
 def format_district(district):
     try:
@@ -293,6 +301,7 @@ def format_district(district):
         return u"District " + district
     except ValueError:
         return district
+
 
 # Not actually used anymore, since we removed the zip codes from display.
 # Please test the first two lines against "01234-4567": it should not return "001234-4567"
@@ -308,6 +317,7 @@ def format_zip(zip):
     except ValueError:
         return zip
 
+
 # Other helpers
 def slugify(text, delim=u'-'):
     """Generates an slightly worse ASCII-only slug."""
@@ -317,6 +327,7 @@ def slugify(text, delim=u'-'):
         if word:
             result.append(word)
     return unicode(delim.join(result))
+
 
 def is_really_iterable(var):
     if not hasattr(var, '__iter__'):
@@ -333,11 +344,18 @@ def is_really_iterable(var):
     else:
         return False
 
+
 def leg_bills_count(leg_id):
     counter = 0
-    context = make_context()
-    for bill in context['COPY']['bills']:
+    copy = get_copy()
+    for bill in copy['bills']:
         if bill['sh_number'] == leg_id:
             counter = counter + 1
 
     return counter
+
+
+def get_copy():
+    if not CACHE.get('copy', None):
+        CACHE['copy'] = copytext.Copy(app_config.COPY_PATH)
+    return CACHE['copy']
